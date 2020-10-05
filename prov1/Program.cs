754c2903919dd84da1571530f88da01a0c9ec9ba
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2020-10-05 09:43:06 +0200

    Porv 1

---
+++ prov1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace prov1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int skott = 0;
+            string boom = "";
+            bool succ = false;
+
+
+            Console.WriteLine("sänka skepp");
+            Console.WriteLine("skriv in en kordinat mellan 1-10");
+
+            while (!succ)
+            {
+                boom = Console.ReadLine();
+                succ = int.TryParse(boom, out skott);
+                if (!succ)
+                {
+                    Console.WriteLine("Det där är inte en siffra");
+                }
+                else if (skott > 10 || skott < 1)
+                {
+                    Console.WriteLine("skriv en kordinat mellan 1-10");
+                }
+            }
+
+            while (skott != 7)
+            {
+                Console.WriteLine("Miss!");
+                Console.WriteLine("testa ingen, skriv en kordinat mellan 1-10");
+                Console.ReadLine();
+            }
+
+            if (skott == 7)
+            {
+                Console.WriteLine("Träff!");
+                Console.WriteLine("Tryck Enter för att stänga ner");
+                Console.ReadLine();
+            }
+
+            //man kan bara skriv svaret rätt första gången
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
